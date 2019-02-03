ddeefa2da5c3a6e7690cd4ee454cafdf54ccaf1a
Raul Rivero <raul9501@gmail.com>
2019-02-03 12:36:17 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,221 @@
+// HTML elements
+let gridContainerHTML = null;
+let playButtonHTML = null;
+let timerHTML = null;
+
+// Global variables
+let rows = 4;
+let cols = 5;
+let numberOfColors = rows * cols / 2;
+let selectedSquares = [];
+let colorPairs = new Map();
+let flipCardsDelayId = null;
+let timerIntervalId = null;
+let gridElements = [];
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+// let colors = generateRandomColors(numberOfColors);
+let colors = ['hotpink', 'darkorchid ', 'purple', 'crimson', 'salmon', 'orange', 'orangered', 'yellow', 'khaki', 'lime', 'green', 'cyan', 'blue', 'maroon', 'black'];
+
+function playButtonClickHandler (event) {
+    event.preventDefault();
+    if (playButtonHTML.innerHTML === 'Start Game' || playButtonHTML.innerHTML === 'Resume Game') {
+        setColorsToGrid();
+        enableGrid();
+        startTimer();
+        playButtonHTML.innerHTML = 'Pause Game';
+    } else {
+        stopTimer();
+        disableGrid();
+        playButtonHTML.innerHTML = 'Resume Game';
+    }
+}
+
+function startTimer() {
+    timerIntervalId = setInterval(updateTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerIntervalId);
+}
+
+function updateTimer() {
+    if (seconds + 1 === 60) {
+        if (minutes + 1 === 60) {
+            hours++;
+        }
+        minutes = minutes < 59 ? minutes + 1 : 0;
+    }
+    seconds = seconds < 59 ? seconds + 1 : 0;
+    timerHTML.innerHTML = formatTimerOutput(hours, minutes, seconds);
+}
+
+function formatTimerOutput(hh, mm, ss) {
+    if (ss < 10) {
+        ss = `0${ss}`;
+    }
+    if (mm < 10) {
+        mm = `0${mm}`;
+    }
+    if (hh < 10) {
+        hh = `0${hh}`;
+    }
+    return `${hh}:${mm}:${ss}`;
+}
+
+function flipperCardOnClickHandler (event) {
+    event.preventDefault();
+    event.target.parentElement.classList.toggle('rotate-onclick');
+    selectedSquares.push(Number(event.target.id.split('-')[2]));
+    let selectedSquaresHTML = [];
+    clearTimeout(flipCardsDelayId);
+    if (selectedSquares.length === 2) {
+        if ((colorPairs.has(selectedSquares[0]) && colorPairs.get(selectedSquares[0]) === selectedSquares[1]) 
+            || (colorPairs.has(selectedSquares[1]) && colorPairs.get(selectedSquares[1]) === selectedSquares[0])) {
+            // colors matched! keep them flipped
+            selectedSquaresHTML = [gridElements[selectedSquares[0]], gridElements[selectedSquares[1]]];
+            selectedSquaresHTML.map(el => el.lastElementChild.disabled = true);
+            selectedSquares = [];
+            if (hasWonGame()) {
+                // user won the game!
+                stopTimer();
+            }
+        } else {
+            selectedSquaresHTML = [gridElements[selectedSquares[0]], gridElements[selectedSquares[1]]];
+            // wrong color pair selection, flip both cards
+            flipCardsDelayId = setTimeout(() => {
+                toggleCards(...selectedSquaresHTML);
+            }, 800);
+        }
+        selectedSquares = [];
+    }
+}
+
+/**
+ * Checks if all the cards are flipped, meaning the user has won the game
+ */
+function hasWonGame() {
+    // check if all cards are flipped
+    return gridElements.every(flipperEl => flipperEl.classList.contains('rotate-onclick'));
+}
+
+/**
+ * Utility function used to flip two cards simultaneously
+ * @param {Element} elem1 - HTML element representing the first card to be flipped
+ * @param {Element} elem2 - HTML element representing the second card to be flipped
+ */
+function toggleCards(elem1, elem2) {
+    elem1.classList.toggle('rotate-onclick');
+    elem2.classList.toggle('rotate-onclick');
+}
+
+/**
+ * Creates an array of random colors to be used as pairs in the grid
+ * @param {Number} amount - Number of random colors to generate
+ */
+function generateRandomColors(amount) {
+    let randomColors = [];
+    for (let i = 0; i < amount; i++) {
+        randomColors.push(`#${((1<<24) * Math.random() | 0).toString(16)}`);
+    }
+    return randomColors;
+}
+
+/**
+ * Generates an arbitrary number between two numbers
+ * @param {Number} min - Lower bound to generate a random number between
+ * @param {Number} max - Upper bound to generate a random number between
+ * @returns {Number} a random number
+ */
+function getRandomNumber(min, max) {
+    var randomFloat = Math.random() * (max - min) + min;
+    return Math.floor(randomFloat);
+}
+
+/**
+ * Creates a squared grid of 'length' rows and columns
+ * @param {Number} length - the number of rows and columns of the grid
+ */
+function createGrid (rows, cols) {
+    let index = 0;
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const flipContainer = document.createElement('div');
+            const flipper = document.createElement('div');
+            flipContainer.classList.add('flip-container');
+            flipper.classList.add('flipper');
+            flipper.onclick = flipperCardOnClickHandler;
+
+            const frontCard = document.createElement('button');
+            frontCard.id = `front-card-${index}`;
+            frontCard.innerHTML = '?';
+            frontCard.classList.add('square-btn__base', 'square-btn__front-card', 'front-card');
+            frontCard.disabled = true;
+            flipper.appendChild(frontCard);
+
+            const backCard = document.createElement('button');
+            backCard.id = `back-card-${index}`;
+            backCard.classList.add('square-btn__base', 'back-card');
+            flipper.appendChild(backCard);
+            gridElements.push(flipper);
+
+            flipContainer.appendChild(flipper);
+            gridContainerHTML.appendChild(flipContainer);
+            index++;
+        }
+    }
+}
+
+/**
+ * Shuffles the elements of an array using random indexes
+ * @param {Array} array - the array whose elements will be shuffled
+ */
+function shuffleNumbersInArray(array) {
+    let tmp = -1; 
+    let current = -1; 
+    let top = array.length;
+    if(top) {
+        while(--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = array[current];
+            array[current] = array[top];
+            array[top] = tmp;
+          }
+    }
+    return array;
+}
+
+function setColorsToGrid() {
+    const gridIndexes = shuffleNumbersInArray([...Array(numberOfColors * 2).keys()]);
+    let index = 0;
+    for (let i = 0; i < numberOfColors; i++) {
+        gridElements[gridIndexes[index]].lastElementChild.style.cssText = `border: 1px solid ${colors[i]}; background: ${colors[i]};`;
+        gridElements[gridIndexes[index + 1]].lastElementChild.style.cssText = `border: 1px solid ${colors[i]}; background: ${colors[i]};`;
+        colorPairs.set(gridIndexes[index], gridIndexes[index + 1]);
+        index += 2;
+    }
+}
+
+function enableGrid() {
+    gridElements.map(square => square.firstChild.disabled = false);
+}
+
+function disableGrid() {
+    gridElements.map(square => square.firstChild.disabled = true);
+}
+
+/**
+ * Initializes all variables that manipulate the DOM
+ */
+function initHTMLVariables () {
+    gridContainerHTML = document.querySelector('.grid-container');
+    playButtonHTML = document.querySelector('.play-btn');
+    timerHTML = document.querySelector('#timer');
+    playButtonHTML.onclick = playButtonClickHandler;
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    initHTMLVariables();
+    createGrid(rows, cols);
+});
